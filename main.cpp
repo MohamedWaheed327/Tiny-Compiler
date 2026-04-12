7d4b7df1350ffbe81aa3468e9d48f3476bf5f0fd
MohamedWaheed327 <mohamedwaheedsa327@gmail.com>
2026-04-12 14:22:36 +0200

    add link for drawing dfa

--- main.cpp
+++ main.cpp
@@ -7,16 +7,17 @@ signed main() {
     cin.tie(0)->sync_with_stdio(0);
 
     auto nfa = merge_rules();
+    // nfa.display();
     auto dfa = NFA_to_DFA(nfa);
     dfa.minimize();
-    dfa.test("if");
-    dfa.test("hellp");
-    dfa.test("55");
-    dfa.test("5a");
-    dfa.test("end");
-    dfa.test("{\"}\"");
-    dfa.test("{dsfodf.;dsgf.d;fsdfjn}");
-    dfa.test("\"{dsfodf.;dsgf.d;fsdfjn}\"");
+    // dfa.test("if");
+    // dfa.test("hellp");
+    // dfa.test("55");
+    // dfa.test("5a");
+    // dfa.test("end");
+    // dfa.test("{\"}\"");
+    // dfa.test("{dsfodf.;dsgf.d;fsdfjn}");
+    // dfa.test("\"{dsfodf.;dsgf.d;fsdfjn}\"");
     // dfa.display();
-    // dfa.generate_graph_code();
+    dfa.generate_graph_code(); // https://dreampuf.github.io/GraphvizOnline
 }

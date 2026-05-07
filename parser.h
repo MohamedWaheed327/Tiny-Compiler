708c100003b5ac86fb1989d98c49ecb3558f91a4
MohamedWaheed327 <mohamed.waheed.327.0@gmail.com>
2026-05-07 12:25:51 +0300

    temp

--- parser.h
+++ parser.h
@@ -26,63 +26,88 @@ string parser(string s, DFA &dfa) {
     };
     remove_comments();
 
-    int n = tokens.size();
-    vector<string> program = tokens;
-    vector<bool> terminal(n, true);
+    deque<string> dq;
+    dq.push_front("S");
+    while (tokens.size()) {
+        auto front = dq.front();
+        dq.pop_front();
 
-    for (int p = 0; p <= 9; ++p) {
-        vector<production_rule> production_rules_;
-        for (auto production_rule : production_rules) {
-            if (production_rule.priority == p) {
-                production_rules_.push_back(production_rule);
-            }
+        if (front == "empty") continue;
+
+        if (isupper(front[0])) {
+            auto pr = pt[{front, tokens.front()}];
+            if (pr.name == "") return "insuccessful";
+            dq.insert(dq.begin(), pr.pattern.begin(), pr.pattern.end());
         }
-
-    redo:;
-        for (int l = 0; l < program.size(); ++l) {
-            for (int r = l; r < program.size(); ++r) {
-                for (auto production_rule : production_rules_) {
-                    if (production_rule.match(program, l, r, terminal, l, r)) {
-                        int last_line = line_number[r];
-                        string temp = accumulate(lexemes.begin() + l, lexemes.begin() + r + 1, string(""), [&](string a, string b) { return a + b; });
-
-                        program.erase(program.begin() + l, program.begin() + r + 1);
-                        terminal.erase(terminal.begin() + l, terminal.begin() + r + 1);
-                        line_number.erase(line_number.begin() + l, line_number.begin() + r + 1);
-                        lexemes.erase(lexemes.begin() + l, lexemes.begin() + r + 1);
-
-                        program.insert(program.begin() + l, production_rule.name);
-                        terminal.insert(terminal.begin() + l, false);
-                        line_number.insert(line_number.begin() + l, last_line);
-                        lexemes.insert(lexemes.begin() + l, temp);
-
-                        goto redo;
-                    }
-                }
-            }
+        else {
+            if (tokens.front() != front) return "insuccessful";
+            tokens.erase(tokens.begin());
         }
     }
 
-    if (program == vector<string>{"S"}) {
-        return "successful";
+    while (dq.size() && first[{dq.front()}].count("empty")) {
+        dq.pop_front();
     }
-    else {
-        stringstream ss;
-        for (int i = 0; i < program.size(); ++i) {
-            if (program[i] == LexicalError) {
-                ss << lexemes[i] << '\n';
-                ss << LexicalError << " at line " << line_number[i] << '\n';
-                ss << "--------------------------------------------------------------------\n";
-            }
-            else {
-                if (program[i] != "S") {
-                    ss << lexemes[i] << '\n';
-                    ss << SyntaxError << " at line " << line_number[i] << '\n';
-                    ss << "--------------------------------------------------------------------\n";
-                }
-            }
-        }
 
-        return ss.str();
-    }
+    return (dq.size() ? "insuccessful" : "successful");
+
+    // int n = tokens.size();
+    // vector<string> program = tokens;
+    // vector<bool> terminal(n, true);
+
+    // for (int p = 0; p <= 9; ++p) {
+    //     vector<production_rule> production_rules_;
+    //     for (auto production_rule : production_rules) {
+    //         if (production_rule.priority == p) {
+    //             production_rules_.push_back(production_rule);
+    //         }
+    //     }
+
+    // redo:;
+    //     for (int l = 0; l < program.size(); ++l) {
+    //         for (int r = l; r < program.size(); ++r) {
+    //             for (auto production_rule : production_rules_) {
+    //                 if (production_rule.match(program, l, r, terminal, l, r)) {
+    //                     int last_line = line_number[r];
+    //                     string temp = accumulate(lexemes.begin() + l, lexemes.begin() + r + 1, string(""), [&](string a, string b) { return a + b; });
+
+    //                     program.erase(program.begin() + l, program.begin() + r + 1);
+    //                     terminal.erase(terminal.begin() + l, terminal.begin() + r + 1);
+    //                     line_number.erase(line_number.begin() + l, line_number.begin() + r + 1);
+    //                     lexemes.erase(lexemes.begin() + l, lexemes.begin() + r + 1);
+
+    //                     program.insert(program.begin() + l, production_rule.name);
+    //                     terminal.insert(terminal.begin() + l, false);
+    //                     line_number.insert(line_number.begin() + l, last_line);
+    //                     lexemes.insert(lexemes.begin() + l, temp);
+
+    //                     goto redo;
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+
+    // if (program == vector<string>{"S"}) {
+    //     return "successful";
+    // }
+    // else {
+    //     stringstream ss;
+    //     for (int i = 0; i < program.size(); ++i) {
+    //         if (program[i] == LexicalError) {
+    //             ss << lexemes[i] << '\n';
+    //             ss << LexicalError << " at line " << line_number[i] << '\n';
+    //             ss << "--------------------------------------------------------------------\n";
+    //         }
+    //         else {
+    //             if (program[i] != "S") {
+    //                 ss << lexemes[i] << '\n';
+    //                 ss << SyntaxError << " at line " << line_number[i] << '\n';
+    //                 ss << "--------------------------------------------------------------------\n";
+    //             }
+    //         }
+    //     }
+
+    //     return ss.str();
+    // }
 }
--- production_rules.h
+++ production_rules.h
@@ -9,6 +9,9 @@ struct production_rule {
     int priority;
 
     production_rule() {}
+    production_rule(string name, vector<string> pattern) : name(name), pattern(pattern) {
+    }
+
     production_rule(string name, vector<string> pattern, vector<bool> terminal, int priority)
         : name(name), pattern(pattern), terminal(terminal), priority(priority) {}
 
@@ -78,5 +81,236 @@ vector<production_rule> get_production_rules() {
 
     return ret;
 }
+
+vector<production_rule> remove_left_recursion() {
+    auto ret = get_production_rules();
+
+    set<string> left_recursion;
+    for (int i = 0; i < ret.size(); ++i) {
+        if (ret[i].name == ret[i].pattern[0]) {
+            left_recursion.insert(ret[i].name);
+        }
+    }
+
+    for (int i = 0; i < ret.size(); ++i) {
+        if (left_recursion.count(ret[i].name)) {
+            if (ret[i].name == ret[i].pattern[0]) {
+                ret[i].pattern.push_back(ret[i].name + '_');
+                ret[i].pattern.erase(ret[i].pattern.begin());
+                ret[i].name += '_';
+            }
+            else {
+                ret[i].pattern.push_back(ret[i].name + '_');
+            }
+        }
+    }
+
+    for (auto lr : left_recursion) {
+        ret.push_back(production_rule(lr + '_', vector<string>{string("empty")}, vector<bool>{true}, 0));
+    }
+    return ret;
+}
+
+map<vector<string>, set<string>> compute_first(vector<production_rule> prs) {
+    map<string, vector<production_rule>> mp;
+    for (auto pr : prs) {
+        mp[pr.name].push_back(pr);
+    }
+
+    map<vector<string>, set<string>> dp;
+    function<set<string>(vector<string>)> get_first = [&](const vector<string> rule) {
+        if (dp.count(rule)) return dp[rule];
+        if (islower(rule[0][0])) return dp[rule] = set<string>{rule[0]};
+
+        set<string> ret;
+        for (auto pr : mp[rule[0]]) {
+            auto get = get_first(pr.pattern);
+            ret.insert(get.begin(), get.end());
+        }
+
+        if (ret.count("empty")) {
+            auto new_rule = rule;
+            new_rule.erase(new_rule.begin());
+            if (new_rule.size()) {
+                ret.erase("empty");
+                auto get = get_first(new_rule);
+                ret.insert(get.begin(), get.end());
+            }
+        }
+
+        return dp[rule] = ret;
+    };
+
+    for (auto pr : prs) {
+        get_first({pr.name});
+    }
+
+    return dp;
+}
+
+map<string, set<string>> compute_follow(vector<production_rule> prs) {
+    map<string, vector<production_rule>> rules;
+    set<string> non_terminals;
+
+    for (auto pr : prs) {
+        rules[pr.name].push_back(pr);
+        non_terminals.insert(pr.name);
+    }
+
+    map<string, set<string>> first;
+
+    for (auto pr : prs) {
+        for (auto x : pr.pattern) {
+            if (islower(x[0])) {
+                first[x].insert(x);
+            }
+            else {
+                non_terminals.insert(x);
+            }
+        }
+    }
+
+    bool changed = true;
+    while (changed) {
+        changed = false;
+
+        for (auto pr : prs) {
+            bool all_empty = true;
+
+            for (auto x : pr.pattern) {
+                int before = first[pr.name].size();
+
+                for (auto y : first[x]) {
+                    if (y != "empty") {
+                        first[pr.name].insert(y);
+                    }
+                }
+
+                if (!first[x].count("empty")) {
+                    all_empty = false;
+                    break;
+                }
+
+                if (first[pr.name].size() != before) {
+                    changed = true;
+                }
+            }
+
+            if (all_empty) {
+                int before = first[pr.name].size();
+                first[pr.name].insert("empty");
+                if (first[pr.name].size() != before) {
+                    changed = true;
+                }
+            }
+        }
+    }
+
+    auto first_of_sequence = [&](vector<string> seq) {
+        set<string> ret;
+
+        if (seq.empty()) {
+            ret.insert("empty");
+            return ret;
+        }
+
+        bool all_empty = true;
+
+        for (auto x : seq) {
+            for (auto y : first[x]) {
+                if (y != "empty") {
+                    ret.insert(y);
+                }
+            }
+
+            if (!first[x].count("empty")) {
+                all_empty = false;
+                break;
+            }
+        }
+
+        if (all_empty) {
+            ret.insert("empty");
+        }
+
+        return ret;
+    };
+
+    map<string, set<string>> follow;
+
+    for (auto nt : non_terminals) {
+        follow[nt];
+    }
+
+    if (!prs.empty()) {
+        follow[prs[0].name].insert("$");
+    }
+
+    changed = true;
+    while (changed) {
+        changed = false;
+
+        for (auto pr : prs) {
+            for (int i = 0; i < pr.pattern.size(); ++i) {
+                if (isupper(pr.pattern[i][0])) {
+                    string B = pr.pattern[i];
+
+                    vector<string> beta;
+                    for (int j = i + 1; j < pr.pattern.size(); ++j) {
+                        beta.push_back(pr.pattern[j]);
+                    }
+
+                    auto first_beta = first_of_sequence(beta);
+
+                    int before = follow[B].size();
+
+                    for (auto x : first_beta) {
+                        if (x != "empty") {
+                            follow[B].insert(x);
+                        }
+                    }
+
+                    if (first_beta.count("empty")) {
+                        for (auto x : follow[pr.name]) {
+                            follow[B].insert(x);
+                        }
+                    }
+
+                    if (follow[B].size() != before) {
+                        changed = true;
+                    }
+                }
+            }
+        }
+    }
+
+    return follow;
+}
+
+auto rules = remove_left_recursion();
+auto first = compute_first(rules);
+auto follow = compute_follow(rules);
+
+map<pair<string, string>, production_rule> construct_parsing_table() {
+    map<pair<string, string>, production_rule> parsing_table;
+
+    for (auto pr : rules) {
+        auto st = first[pr.pattern];
+        for (auto it : st) {
+            parsing_table[{pr.name, it}] = pr;
+        }
+        if (st.count("empty")) {
+            auto st = follow[pr.name];
+            for (auto it : st) {
+                if (parsing_table.count({pr.name, it}) == 0 or pr.pattern != vector<string>{"empty"})
+                    parsing_table[{pr.name, it}] = pr;
+            }
+        }
+    }
+
+    return parsing_table;
+}
+
+auto pt = construct_parsing_table();
 
 vector<production_rule> production_rules = get_production_rules();

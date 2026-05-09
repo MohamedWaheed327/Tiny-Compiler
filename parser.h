caa0cb724c7d347756c287e42eb03fbb1e81ae42
MohamedWaheed327 <mohamed.waheed.327.0@gmail.com>
2026-05-09 18:28:42 +0300

    added LL(1)

--- parser.h
+++ parser.h
@@ -26,63 +26,130 @@ string parser(string s, DFA &dfa) {
     };
     remove_comments();
 
-    int n = tokens.size();
-    vector<string> program = tokens;
-    vector<bool> terminal(n, true);
+    deque<string> dq;
+    dq.push_front("S");
 
-    for (int p = 0; p <= 9; ++p) {
-        vector<production_rule> production_rules_;
-        for (auto production_rule : production_rules) {
-            if (production_rule.priority == p) {
-                production_rules_.push_back(production_rule);
-            }
+    stringstream ss;
+    int last = 1;
+    vector<int> lines;
+
+    while (tokens.size()) {
+
+        if (tokens[0] == LexicalError) {
+            ss << line_number[0] << " lex" << '\n';
+            last = line_number[0];
+            tokens.erase(tokens.begin());
+            line_number.erase(line_number.begin());
+            continue;
         }
 
-    redo:;
-        for (int l = 0; l < program.size(); ++l) {
-            for (int r = l; r < program.size(); ++r) {
-                for (auto production_rule : production_rules_) {
-                    if (production_rule.match(program, l, r, terminal, l, r)) {
-                        int last_line = line_number[r];
-                        string temp = accumulate(lexemes.begin() + l, lexemes.begin() + r + 1, string(""), [&](string a, string b) { return a + b; });
+        auto front = dq.front();
+        dq.pop_front();
 
-                        program.erase(program.begin() + l, program.begin() + r + 1);
-                        terminal.erase(terminal.begin() + l, terminal.begin() + r + 1);
-                        line_number.erase(line_number.begin() + l, line_number.begin() + r + 1);
-                        lexemes.erase(lexemes.begin() + l, lexemes.begin() + r + 1);
+        if (front == "empty") continue;
 
-                        program.insert(program.begin() + l, production_rule.name);
-                        terminal.insert(terminal.begin() + l, false);
-                        line_number.insert(line_number.begin() + l, last_line);
-                        lexemes.insert(lexemes.begin() + l, temp);
+        if (isupper(front[0])) {
 
-                        goto redo;
-                    }
+            if (parsing_table.count({front, tokens.front()}) == 0) {
+                ss << line_number[0] << '\n';
+                dq.push_front(front);
+                last = line_number[0];
+                tokens.erase(tokens.begin());
+                line_number.erase(line_number.begin());
+                continue;
+            }
+
+            auto pr = parsing_table[{front, tokens.front()}];
+
+            if (pr.name == "synch") {
+                if (front == "S") {
+                    ss << line_number[0] << '\n';
+                    dq.push_front(front);
+                    last = line_number[0];
+                    tokens.erase(tokens.begin());
+                    line_number.erase(line_number.begin());
+                }
+                else {
+                    ss << last << '\n';
+                    continue;
                 }
             }
+
+            dq.insert(dq.begin(), pr.pattern.begin(), pr.pattern.end());
+        }
+        else {
+            if (tokens.front() != front) {
+                ss << last << '\n';
+                continue;
+            }
+            last = line_number[0];
+            tokens.erase(tokens.begin());
+            line_number.erase(line_number.begin());
         }
     }
 
-    if (program == vector<string>{"S"}) {
-        return "successful";
+    while (dq.size() && count(first[{dq.front()}].begin(), first[{dq.front()}].end(), "empty")) {
+        dq.pop_front();
     }
-    else {
-        stringstream ss;
-        for (int i = 0; i < program.size(); ++i) {
-            if (program[i] == LexicalError) {
-                ss << lexemes[i] << '\n';
-                ss << LexicalError << " at line " << line_number[i] << '\n';
-                ss << "--------------------------------------------------------------------\n";
-            }
-            else {
-                if (program[i] != "S") {
-                    ss << lexemes[i] << '\n';
-                    ss << SyntaxError << " at line " << line_number[i] << '\n';
-                    ss << "--------------------------------------------------------------------\n";
-                }
-            }
-        }
 
-        return ss.str();
-    }
+    return ss.str().size() ? ss.str() : "ok";
+
+    // return (dq.size() ? "insuccessful" : "successful");
+
+    // int n = tokens.size();
+    // vector<string> program = tokens;
+
+    // for (int p = 0; p <= 9; ++p) {
+    //     vector<production_rule> production_rules_;
+    //     for (auto production_rule : production_rules) {
+    //         if (production_rule.priority == p) {
+    //             production_rules_.push_back(production_rule);
+    //         }
+    //     }
+
+    // redo:;
+    //     for (int l = 0; l < program.size(); ++l) {
+    //         for (int r = l; r < program.size(); ++r) {
+    //             for (auto production_rule : production_rules_) {
+    //                 if (production_rule.match(program, l, r)) {
+    //                     int last_line = line_number[r];
+    //                     string temp = accumulate(lexemes.begin() + l, lexemes.begin() + r + 1, string(""), [&](string a, string b) { return a + b; });
+
+    //                     program.erase(program.begin() + l, program.begin() + r + 1);
+    //                     line_number.erase(line_number.begin() + l, line_number.begin() + r + 1);
+    //                     lexemes.erase(lexemes.begin() + l, lexemes.begin() + r + 1);
+
+    //                     program.insert(program.begin() + l, production_rule.name);
+    //                     line_number.insert(line_number.begin() + l, last_line);
+    //                     lexemes.insert(lexemes.begin() + l, temp);
+
+    //                     goto redo;
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+
+    // if (program == vector<string>{"S"}) {
+    //     return "successful";
+    // }
+    // else {
+    //     stringstream ss;
+    //     for (int i = 0; i < program.size(); ++i) {
+    //         if (program[i] == LexicalError) {
+    //             ss << lexemes[i] << '\n';
+    //             ss << LexicalError << " at line " << line_number[i] << '\n';
+    //             ss << "--------------------------------------------------------------------\n";
+    //         }
+    //         else {
+    //             if (program[i] != "S") {
+    //                 ss << lexemes[i] << '\n';
+    //                 ss << SyntaxError << " at line " << line_number[i] << '\n';
+    //                 ss << "--------------------------------------------------------------------\n";
+    //             }
+    //         }
+    //     }
+
+    //     return ss.str();
+    // }
 }
--- production_rules.h
+++ production_rules.h
@@ -5,28 +5,26 @@ using namespace std;
 struct production_rule {
     string name;
     vector<string> pattern;
-    vector<bool> terminal;
-    int priority;
 
     production_rule() {}
-    production_rule(string name, vector<string> pattern, vector<bool> terminal, int priority)
-        : name(name), pattern(pattern), terminal(terminal), priority(priority) {}
+    production_rule(string name, vector<string> pattern)
+        : name(name), pattern(pattern) {}
 
-    bool match(vector<string> a, vector<bool> b) {
-        if (a.size() != b.size() or a.size() != pattern.size()) {
+    bool match(vector<string> a) {
+        if (a.size() != pattern.size()) {
             return false;
         }
 
         for (int i = 0; i < a.size(); ++i) {
-            if (a[i] != pattern[i] or b[i] != terminal[i]) {
+            if (a[i] != pattern[i]) {
                 return false;
             }
         }
         return true;
     }
 
-    bool match(vector<string> a, int l1, int r1, vector<bool> b, int l2, int r2) {
-        return match(vector<string>(a.begin() + l1, a.begin() + r1 + 1), vector<bool>(b.begin() + l2, b.begin() + r2 + 1));
+    bool match(vector<string> a, int l, int r) {
+        return match(vector<string>(a.begin() + l, a.begin() + r + 1));
     }
 };
 
@@ -47,18 +45,16 @@ vector<string> split(string s, char c) { // spaces trimmed
 }
 
 vector<production_rule> get_production_rules() {
-    string path = "../assets/grammer_rules.yml";
+    string path = "../assets/grammer_rules (lr, lf removed).yml";
     fstream istr;
     istr.open(path, ios_base::openmode::_S_in);
 
-    int priority = 0;
     vector<production_rule> ret;
     while (!istr.eof()) {
         string line;
         getline(istr, line);
 
         if (line.empty()) {
-            priority++;
             continue;
         }
 
@@ -66,12 +62,7 @@ vector<production_rule> get_production_rules() {
         auto b = split(a[1], '|');
 
         for (auto rule_piece : b) {
-            auto c = split(rule_piece, '.');
-            vector<bool> d;
-            for (auto it : c) {
-                d.push_back(islower(it[0]));
-            }
-            ret.push_back(production_rule(a[0], c, d, priority));
+            ret.push_back(production_rule(a[0], split(rule_piece, '.')));
         }
     }
     istr.close();
@@ -79,4 +70,115 @@ vector<production_rule> get_production_rules() {
     return ret;
 }
 
+map<vector<string>, vector<string>> get_first() {
+    string path = "../assets/first.yml";
+    fstream istr;
+    istr.open(path, ios_base::openmode::_S_in);
+
+    map<vector<string>, vector<string>> ret;
+    while (!istr.eof()) {
+        string line;
+        getline(istr, line);
+
+        if (line.empty()) {
+            continue;
+        }
+
+        auto a = split(line, ':');
+        auto b = split(a[0], '.');
+
+        a[1] = a[1].substr(1, a[1].size() - 2);
+
+        ret[b] = split(a[1], ',');
+    }
+    istr.close();
+
+    return ret;
+}
+
+map<string, vector<string>> get_follow() {
+    string path = "../assets/follow.yml";
+    fstream istr;
+    istr.open(path, ios_base::openmode::_S_in);
+
+    map<string, vector<string>> ret;
+    while (!istr.eof()) {
+        string line;
+        getline(istr, line);
+
+        if (line.empty()) {
+            continue;
+        }
+
+        auto a = split(line, ':');
+        a[1] = a[1].substr(1, a[1].size() - 2);
+        ret[a[0]] = split(a[1], ',');
+    }
+    istr.close();
+
+    return ret;
+}
+
 vector<production_rule> production_rules = get_production_rules();
+map<vector<string>, vector<string>> first = get_first();
+map<string, vector<string>> follow = get_follow();
+
+map<pair<string, string>, production_rule> construct_parsing_table() {
+    map<pair<string, string>, production_rule> parsing_table;
+
+    for (auto pr : production_rules) {
+        auto st = set<string>(first[pr.pattern].begin(), first[pr.pattern].end());
+        for (auto it : st) {
+            if (it != "empty") {
+                parsing_table[{pr.name, it}] = pr;
+            }
+        }
+        if (st.count("empty")) {
+            auto st = follow[pr.name];
+            for (auto it : st) {
+                if (parsing_table.count({pr.name, it}) == 0 or pr.pattern != vector<string>{"empty"} or parsing_table[{pr.name, it}].name == "synch")
+                    parsing_table[{pr.name, it}] = pr;
+            }
+        }
+        else {
+            auto st = follow[pr.name];
+            for (auto it : st) {
+                if (parsing_table.count({pr.name, it}) == 0)
+                    parsing_table[{pr.name, it}] = production_rule("synch", {});
+            }
+        }
+    }
+
+    return parsing_table;
+}
+
+map<pair<string, string>, production_rule> parsing_table = construct_parsing_table();
+
+/*
+
+x := (x + (x + (y - x / (x / 5- x * 5))));
+read x, y, z;
+write x, y, z;
+
+if ((x * x = 5 * 5)) then
+    f := 5;
+    repeat
+    until x = 5;
+
+    x := (x + (x + (y - x / (x / 5- x * 5))));
+    read x, y, z;
+    write x, y, z;
+
+    if x = x then
+        x := (x + (x + (y - x / (x / 5- x * 5))));
+        read x, y, z;
+        write x, y, z;
+    else
+        x := (x + (x + (y - x / (x / 5- x * 5))));
+        read x, y, z;
+        write x, y, z;
+    end;
+    write "nikfndiofn rfdnfonertif", x, y, "jbjbfdf";
+end;
+
+*/
